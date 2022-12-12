dfa51581c41f2126bb956937ab3ebc9f665c29c9
vgaidukov <vgaidukov@gmail.com>
2022-12-12 22:57:44 +0300

    feat: set Section and Card components for Account tab

---
+++ src/components/AccountSection.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="section">
+    <ion-toolbar>
+      <ion-title class="section__title">{{ name }}</ion-title>
+    </ion-toolbar>
+    <div :class=this.className>
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {
+  IonToolbar,
+  IonTitle,
+} from '@ionic/vue';
+
+export default defineComponent({
+  name: 'AccountSection',
+  props: {
+    name: String,
+    className: String,
+  },
+  components: {
+    IonToolbar,
+    IonTitle,
+  },
+});
+</script>
+
+<style scoped>
+/* iOS places the subtitle above the title */
+ion-card-header.ios {
+  display: flex;
+  flex-flow: column-reverse;
+}
+
+ion-card {
+  position: relative;
+}
+
+ion-badge {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  padding: 10px;
+  border-radius: 50%;
+  opacity: .9;
+}
+
+ion-card-content {
+  padding-top: 0;
+}
+
+.section__title {
+  font-size: 120%;
+}
+
+@media (orientation: landscape) {
+  .votes-section, .my-communities-section {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+</style>
---
+++ src/components/AccountSectionCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <ion-card class="section-card" @click=console>
+    <ion-card-header>
+      <ion-card-title>{{ title }}</ion-card-title>
+      <ion-card-subtitle>{{ subtitle }}</ion-card-subtitle>
+    </ion-card-header>
+    <ion-card-content>
+      <slot></slot>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script lang="ts">
+  import {defineComponent} from 'vue';
+  import {
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+  } from '@ionic/vue';
+
+  export default defineComponent({
+    name: 'AccountSectionCard',
+    props: {
+      // cardColor: String,
+      title: String,
+      subtitle: String,
+      content: String,
+    },
+    components: {
+      IonCard,
+      IonCardContent,
+      IonCardHeader,
+      IonCardSubtitle,
+      IonCardTitle,
+    },
+    methods: {
+      console() {
+        console.log(1000);
+      }
+    }
+  });
+</script>
+
+<style scoped>
+  /* iOS places the subtitle above the title */
+  ion-card-header.ios {
+    display: flex;
+    flex-flow: column-reverse;
+  }
+
+  .section-card {
+    box-shadow: 3px 3px 7px #666666;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    .section-card {
+      box-shadow: none;
+    }
+  }
+</style>
