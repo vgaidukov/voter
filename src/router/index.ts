882d8d58954f0fc0d6acb64206f25d84637cd102
vgaidukov <vgaidukov@gmail.com>
2022-12-15 23:11:55 +0300

    set tab routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/tabs/account'
   },
   {
     path: '/tabs/',
@@ -13,19 +13,19 @@ const routes: Array<RouteRecordRaw> = [
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        redirect: '/tabs/account'
       },
       {
-        path: 'tab1',
+        path: 'account',
         component: () => import('@/views/TabAccount.vue')
       },
       {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        path: 'communities',
+        component: () => import('@/views/TabCommunities.vue')
       },
       {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        path: 'votes',
+        component: () => import('@/views/TabVotes.vue')
       }
     ]
   }
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,17 +3,17 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
+        <ion-tab-button tab="account" href="/tabs/account">
           <ion-icon :icon="personCircleOutline" />
           <ion-label>Профиль</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
+        <ion-tab-button tab="communities" href="/tabs/communities">
           <ion-icon :icon="peopleOutline" />
           <ion-label>Сообщества</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
+        <ion-tab-button tab="votes" href="/tabs/votes">
           <ion-icon :icon="checkmarkDoneCircleOutline" />
           <ion-label>Голосования</ion-label>
         </ion-tab-button>
