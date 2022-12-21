e8d957d8590c9a36c2bf326db835fde6a68ed196
vgaidukov <vgaidukov@gmail.com>
2022-12-21 23:45:37 +0300

    scroll section layout

--- src/components/ScrollSection.vue
+++ src/components/ScrollSection.vue
@@ -5,7 +5,7 @@
         <ion-icon :icon="manOutline"/>
         <p>10</p>
       </div>
-      <img class="community__image" :src="'https://picsum.photos/100/100?random=' + index" alt="avatar"/>
+      <img class="community__image" :src="'https://picsum.photos/200/200?random=' + index" alt="avatar"/>
       <div class="community__label">
         <h3 class="community__title">{{ item }}</h3>
         <p class="community__description">Какая-то общедоступная информация о сообществе</p>
@@ -71,37 +71,6 @@ p, h3, ul, li {
   padding: 0;
 }
 
-.add-button {
-  padding: 0;
-  margin: 0;
-  width: 25px;
-  height: 25px;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #5598ff;
-  border-radius: 50%;
-  box-shadow: 1px 1px 7px #989aa2;
-}
-
-.add-button::before {
-  position: absolute;
-  content: "";
-  width: 12px;
-  border-bottom: 1px solid white;
-}
-
-.add-button::after {
-  position: absolute;
-  content: "";
-  width: 12px;
-  border-bottom: 1px solid white;
-  rotate: 90deg;
-}
-
 .communities {
   display: flex;
   flex-direction: column;
@@ -118,22 +87,29 @@ p, h3, ul, li {
   align-items: center;
   border-radius: 15px;
   box-shadow: 0 1px 10px #989aa2;
+  overflow: hidden;
 }
 
 .community__label {
   margin: 0 0 0 15px;
+  z-index: 3;
 }
 
 .community__image {
-  margin: 10px;
+  margin: 0 10px 0 0;
   padding: 0;
-  border-radius: 50%;
-  box-shadow: 0 3px 10px #666666;
+  width: 120px;
   position: relative;
+  z-index: 3;
 }
 
 .community__title {
-  font-weight: lighter;
+  /*font-weight: lighter;*/
+  /*text-transform: ;*/
+  font-weight: bold;
+  font-size: 1.2em;
+  letter-spacing: -0.5px;
+  margin-bottom: 5px;
 }
 
 .community__description {
@@ -152,13 +128,45 @@ p, h3, ul, li {
   align-items: center;
   background-color: white;
   border-radius: 50%;
-  box-shadow: 1px 1px 7px #989aa2;
+  box-shadow: 1px 1px 7px;
   opacity: .7;
-  z-index: 5;
+  z-index: 4;
 
   font-size: .8em;
   text-align: center;
   color: #0d0d0d;
+}
+
+.add-button {
+  padding: 0;
+  margin: 0;
+  width: 25px;
+  height: 25px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #5598ff;
+  border-radius: 50%;
+  box-shadow: 1px 1px 7px #989aa2;
+  z-index: 3;
+}
+
+.add-button::before {
+  position: absolute;
+  content: "";
+  width: 12px;
+  border-bottom: 1px solid white;
+}
+
+.add-button::after {
+  position: absolute;
+  content: "";
+  width: 12px;
+  border-bottom: 1px solid white;
+  rotate: 90deg;
 }
 
 @media (prefers-color-scheme: dark) {
@@ -167,7 +175,8 @@ p, h3, ul, li {
   }
 
   .community {
-    box-shadow: 1px 1px 200px #5598ff;
+    box-shadow: 1px 1px 120px #5598ff;
+    /*box-shadow: none;*/
   }
 }
 
