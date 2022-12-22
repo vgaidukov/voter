c8a820f9c6dd36d1adf450c738436d1f35546ef3
vgaidukov <vgaidukov@gmail.com>
2022-12-22 23:15:42 +0300

    change Tabs names

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,9 +3,9 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="account" href="/tabs/account">
-          <ion-icon :icon="personCircleOutline" />
-          <ion-label>Профиль</ion-label>
+        <ion-tab-button tab="main" href="/tabs/main">
+          <ion-icon :icon="gridOutline" />
+          <ion-label>Главная</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="communities" href="/tabs/communities">
@@ -25,16 +25,19 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { personCircleOutline, peopleOutline, checkmarkDoneCircleOutline } from 'ionicons/icons';
+import { personCircleOutline, peopleOutline, checkmarkDoneCircleOutline, gridOutline } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
-  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
+  components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon,
+    IonPage,
+    IonRouterOutlet },
   setup() {
     return {
       personCircleOutline,
       peopleOutline,
-      checkmarkDoneCircleOutline
+      checkmarkDoneCircleOutline,
+      gridOutline
     }
   }
 });
