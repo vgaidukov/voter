e467e1def528aef21bc86f5cea8a2bf60c0b1612
vgaidukov <vgaidukov@gmail.com>
2022-12-22 23:18:24 +0300

    feat: add segment buttons and searchbar

--- src/views/TabCommunities.vue
+++ src/views/TabCommunities.vue
@@ -1,44 +1,83 @@
 <template>
   <ion-page>
+
     <ion-header :translucent="true">
       <ion-toolbar class="header__toolbar">
-        <ion-title class="header__title" size="large">Мои сообщества</ion-title>
-        <!--        <ion-buttons :collapse="true" slot="primary" class="header__buttons">-->
-        <!--          <ion-button class="header__button" fill="outline">-->
-        <!--            &lt;!&ndash;            <ion-icon slot="end" :icon="create"></ion-icon>&ndash;&gt;-->
-        <!--            <p class="header__button-label">Изменить</p>-->
-        <!--          </ion-button>-->
-        <!--        </ion-buttons>-->
+        <ion-segment value="self">
+          <ion-segment-button value="self">
+            <ion-label>Мои сообщества</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="all">
+            <ion-label>Все сообщества</ion-label>
+          </ion-segment-button>
+        </ion-segment>
       </ion-toolbar>
+      <ion-searchbar></ion-searchbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Мои сообщества</ion-title>
-        </ion-toolbar>
-      </ion-header>
-        <scroll-section>
-        </scroll-section>
+      <community-list></community-list>
+      <!--      <my-community-list></my-community-list>-->
     </ion-content>
+
   </ion-page>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
-import {IonPage, IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/vue';
-import ScrollSection from "@/components/ScrollSection.vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonContent,
+  IonSearchbar,
+  IonLabel,
+  IonSegment,
+  IonSegmentButton,
+  // IonIcon
+} from '@ionic/vue';
+import CommunityList from "@/components/CommunityList.vue";
+// import MyCommunityList from "@/components/MyCommunityList.vue";
+// import {add} from 'ionicons/icons';
+
 
 export default defineComponent({
   name: 'TabCommunities',
   components: {
+    CommunityList,
+    // MyCommunityList,
     IonHeader,
     IonToolbar,
-    IonTitle,
     IonContent,
     IonPage,
-    ScrollSection
+    IonSearchbar,
+    IonLabel,
+    IonSegment,
+    IonSegmentButton,
+    // IonIcon
+  },
+  setup() {
+    return {
+      // add
+    }
   }
 });
 </script>
+
+<style scoped>
+ion-searchbar .md {
+  margin-bottom: 5px;
+}
+
+ion-segment {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+@media (prefers-color-scheme: dark) {
+  .ios ion-segment-button::part(indicator-background) {
+    background-color: rgba(85, 152, 255, .4);
+  }
+}
+</style>
 
