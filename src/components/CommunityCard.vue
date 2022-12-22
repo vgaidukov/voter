45d8d16a444f42a825c298dda440eea5fb822f88
vgaidukov <vgaidukov@gmail.com>
2022-12-22 23:19:27 +0300

    refact and layout list of communities

---
+++ src/components/CommunityCard.vue
@@ -0,0 +1,151 @@
+<template>
+  <li class="community">
+    <div class="badge">
+      <ion-icon :icon="manOutline"/>
+      <p>10</p>
+    </div>
+    <img class="community__image" :src="'https://picsum.photos/100/100?random=' + this.$attrs.index" alt="avatar"/>
+    <div class="community__label">
+      <h3 class="community__title">{{ this.$attrs.item }}</h3>
+      <p class="community__description">Какая-то общедоступная информация о сообществе</p>
+    </div>
+    <ion-button class="add-button" shape="round" fill="clear">
+    </ion-button>
+  </li>
+</template>
+
+<script lang="ts">
+import {
+  IonButton,
+  IonIcon
+} from '@ionic/vue';
+import {defineComponent} from 'vue';
+import {manOutline} from 'ionicons/icons';
+
+export default defineComponent({
+  name: 'CommunityCard',
+  props: {
+
+  },
+  components: {
+    IonButton,
+    IonIcon
+  },
+  setup: function () {
+
+    return {manOutline};
+  },
+});
+</script>
+
+<style scoped>
+p, h3, li, ion-icon {
+  margin: 0;
+  padding: 0;
+}
+
+.community {
+  padding-right: 15px;
+  margin: 12px 0 0 0;
+  min-height: 100px;
+  width: 92%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  border-radius: 15px;
+  box-shadow: 0 1px 10px #989aa2;
+  overflow: hidden;
+}
+
+.community__label {
+  z-index: 3;
+}
+
+.community__image {
+  margin: 0 15px 0 0;
+  padding: 0;
+  width: 100px;
+  position: relative;
+  z-index: 3;
+}
+
+.community__title {
+  font-weight: bold;
+  font-size: 1.1em;
+  letter-spacing: -0.5px;
+  margin-bottom: 5px;
+}
+
+.community__description {
+  font-size: 80%;
+}
+
+.badge {
+  padding: 0 5px;
+  border-radius: 20px;
+  min-width: 40px;
+  height: 40px;
+  position: absolute;
+  top: 7px;
+  left: 7px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  box-shadow: 1px 1px 7px;
+  opacity: .7;
+  z-index: 4;
+
+  font-size: .8em;
+  text-align: center;
+  color: #0d0d0d;
+}
+
+.add-button {
+  padding: 0;
+  margin: 0;
+  width: 25px;
+  height: 25px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #5598ff;
+  border-radius: 50%;
+  box-shadow: 1px 1px 7px #989aa2;
+  z-index: 3;
+}
+
+.add-button::before {
+  position: absolute;
+  content: "";
+  width: 12px;
+  border-bottom: 1px solid white;
+}
+
+.add-button::after {
+  position: absolute;
+  content: "";
+  width: 12px;
+  border-bottom: 1px solid white;
+  rotate: 90deg;
+}
+
+@media (prefers-color-scheme: dark) {
+  .community__image, .add-button {
+    box-shadow: none;
+  }
+
+  .community {
+    box-shadow: 1px 1px 120px #5598ff;
+  }
+}
+
+@media screen and (min-width: 666.98px) {
+  .community {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/MyCommunityList.vue
@@ -0,0 +1,81 @@
+<template>
+  <ul>
+    <my-community-card
+      v-for="community in communities"
+      :color="community.color"
+      :community="community"
+      v-bind:key="community"
+      :imgSrc="community.imgUrl">
+    </my-community-card>
+  </ul>
+  <ion-fab slot="fixed" vertical="bottom" horizontal="end">
+    <ion-fab-button size="small">
+      <ion-icon :icon="add"></ion-icon>
+    </ion-fab-button>
+  </ion-fab>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {manOutline, add} from 'ionicons/icons';
+import {IonIcon} from '@ionic/vue';
+import MyCommunityCard from "@/components/MyCommunityCard.vue";
+
+export default defineComponent({
+  name: 'MyCommunityList',
+  components: {
+    MyCommunityCard,
+    IonIcon
+
+  },
+  setup() {
+    return {
+      manOutline,
+      add,
+      communities: [
+        {
+          color: "primary",
+          title: "Рыбинские",
+          subtitle: "Братство",
+          participants: 5,
+          lastVote: "Чо с новым годом?",
+          imgUrl: "https://picsum.photos/500/500?random=121"
+        },
+        {
+          color: "secondary",
+          title: "Московские",
+          subtitle: "Товарищество",
+          participants: 20,
+          lastVote: "Во сколько собираемся в воскр?",
+          imgUrl: "https://picsum.photos/500/500?random=123"
+        },
+        {
+          color: "tertiary",
+          title: "Питерские",
+          subtitle: "Коммуна",
+          participants: 1000,
+          lastVote: "Го по пифку?",
+          imgUrl: "https://picsum.photos/500/500?random=125"
+        }]
+    };
+  }
+})
+;
+</script>
+
+<style scoped>
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+@media screen and (min-width: 666.98px) {
+  .communities {
+    width: 98%;
+    margin: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 15px;
+  }
+}
+</style>
